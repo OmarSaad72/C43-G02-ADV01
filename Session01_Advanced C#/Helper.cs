7e07cacb79e3eadb41e98bab9fd506d3c0d84557
OmarSaad <OmarSaad@DESKTOP-2C0MESG>
2025-01-07 16:44:03 +0200

    Video01_Generics

---
+++ Session01_Advanced C#/Helper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session01_Advanced_C_
+{
+    internal class Helper<T>
+    {
+        public static void Swap<T>(ref T x, ref T y)
+        {
+            T Temp = x;
+            x = y;
+            y = Temp;
+        }
+        //public static void Swap (ref int x , ref int y )
+        //{
+        //    int Temp = x;
+        //    x = y;
+        //    y = Temp;
+        //}
+        //public static void Swap(ref double x, ref double y)
+        //{
+        //    double Temp = x;
+        //    x = y;
+        //    y = Temp;
+        //}
+        //public static void Swap(ref Point x, ref Point y)
+        //{
+        //    Point Temp = x;
+        //    x = y;
+        //    y = Temp;
+        //}
+    }
+}
--- Session01_Advanced C#/Program.cs
+++ Session01_Advanced C#/Program.cs
@@ -2,9 +2,37 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            #region Generics
+            int A = 8, B = 10;
+            Console.WriteLine(A);
+            Console.WriteLine(B);
+            Console.WriteLine("After Swap: ");
+            Helper<int>.Swap(ref A, ref B);
+            Console.WriteLine(A);
+            Console.WriteLine(B);
+            Console.WriteLine("***********************");
+            double x = 10.5, y = 20.5;
+            //double x = 10.5, y = 20;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine("After Swap: ");
+            Helper<double>.Swap(ref x, ref y);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine("***********************");
+            Point p1 = new Point(10, 20);
+            Point p2 = new Point(30, 40);
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine("After Swap: ");
+            Helper<Point>.Swap(ref p1, ref p2);
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            #endregion
+
         }
     }
 }
+
