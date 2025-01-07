c5420b69e482fc89d53dfb0b9de1a5a637c0c52b
OmarSaad <OmarSaad@DESKTOP-2C0MESG>
2025-01-07 16:15:11 +0200

    Add project files.

---
+++ Session01_Advanced C#/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Session01_Advanced_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
